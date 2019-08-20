55bfff8df4b27937bfe296cf0f9fc31dcb9db03d
MinysJava <poforli@gmail.com>
2019-08-21 00:29:31 +0500

    Задание №2

--- src/Lesson_1/ArrayElemChange.java
+++ src/Lesson_1/ArrayElemChange.java
@@ -14,7 +14,7 @@ public class ArrayElemChange<T> {
         this.v1 = v1;
         this.arr = arr;
     }
-
+//-----------------Меняем элементы----------------------------------
     public T[] change(int v1, int v2, T[] arr){
 
         T arrX;
@@ -25,9 +25,19 @@ public class ArrayElemChange<T> {
 
         return arr;
     }
+//---------------Конвертер--------------------------------
+    public ArrayList convert(T[] arr){
+        ArrayList arrResult = new ArrayList(arr.length);
+
+        for (int i = 0; i < arr.length; i++){
+            arrResult.add(i, arr[i]);
+        }
+        return arrResult;
+    }
 
     public static void main(String[] args) {
 
+//-----------------Задание №1-----------------------------------------------------------
        ArrayElemChange<ArrayList> arrayListMain = new ArrayElemChange<>();
        ArrayList array[] = new ArrayList[3];
        ArrayList resultArray[] = arrayListMain.change(0, 2, array);
@@ -35,5 +45,14 @@ public class ArrayElemChange<T> {
        ArrayElemChange<List> listMain = new ArrayElemChange<>();
        List list[] = new List[3];
        list = listMain.change(1, 2, list);
+
+//-----------------Задание №2-----------------------------------------------------------
+       List intArr[] = new List[3];
+       ArrayElemChange<List> intConvert = new ArrayElemChange<>();
+       ArrayList convetArr = intConvert.convert(intArr);
+       System.out.println("Было: " + intArr.getClass().getName());
+       System.out.println("Стало: " + convetArr.getClass().getName());
+
+
     }
 }
